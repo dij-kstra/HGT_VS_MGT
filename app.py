53837e38f843e7a0b6f9aeb42b05800cd34bfdec
_dijkstra <_dijkstra@macbooks-MacBook-Air.local>
2024-02-03 19:32:35 +0530

    app.py updated

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 # app.py
 import streamlit as st
 from logistic_regression import predict_logistic_regression
-#from dummy import predict_dummy
+# from dummy import predict_dummy
 
 st.title("Text Classification Application")
-st.write('HGT VS MGT')
+st.markdown("Welcome to the Text Classification Application, where you can predict whether a text is Human Generated or Machine Generated. You can select the model of your choice from the sidebar.")
 
 # User input
 text_input = st.text_area("Enter your text here:", "")
 
-#Model selection
+# Model selection
+st.sidebar.header("HGT VS MGT Application")
 model_option = st.sidebar.selectbox("Select Model", ["Logistic Regression", "Dummy Model"])
+
+# Initialize prediction variable
+prediction = None
 
 # Prediction
 if st.button("Predict"):
@@ -21,4 +25,7 @@ if st.button("Predict"):
     else:
         st.error("Invalid model selection")
 
-    st.success(f"Prediction: {prediction}")
+# Check if prediction is not None before using it
+if prediction is not None:
+    prediction_result = "Human Generated text" if prediction[0] == 0 else "Machine Generated Text"
+    st.success(f"Prediction: {prediction_result}")
